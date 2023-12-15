35f5785f123f05f5911be774074965bfcedf84cc
sahil556 <sahilnandaniya556@gmail.com>
2023-12-15 15:10:09 +0530

    comment on post and subscription related components are generated.

--- tech-blog-app/src/app/app.module.ts
+++ tech-blog-app/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { SingleCategoryComponent } from './pages/single-category/single-category
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
     SingleCategoryComponent,
     SinglePostComponent,
     TermsAndConditionComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    CommentFormComponent,
+    CommentListComponent
   ],
   imports: [
     BrowserModule,
